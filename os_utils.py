1a3767ec4e040a5fb7ff7649cce71ef6cc9a5fa2
Misaka <1198890085@qq.com>
2024-10-12 16:13:48 +0800

    web后置脚本

--- os_utils.py
+++ os_utils.py
@@ -1,4 +1,4 @@
-# encoding: utf-8
+import locale
 import subprocess
 from typing import Union
 
@@ -9,10 +9,18 @@ def run(cmd: Union[list, str]):
     if isinstance(cmd, list):
         log.info(f"Run command: {' '.join(cmd)}")
     else:
+        cmd = cmd.replace("\r", "").replace("\n", "&&")
         log.info(f"Run command: {cmd}")
     try:
-        popen = subprocess.Popen(cmd, shell=True)
+        popen = subprocess.Popen(cmd, shell=True,
+                                 encoding=locale.getpreferredencoding(),
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = popen.communicate()
+        if stdout:
+            log.info(stdout)
+        if stderr:
+            log.error(stderr)
     except subprocess.SubprocessError as e:
-        log.error(f"Run command error: {e}")
+        log.exception(e)
         return None, str(e)
     return popen.pid, None
